ffda21781928917150b21f6805980eaa4ae80bea
Faheem <feemag.dev@hotmail.com>
2021-12-16 10:55:36 +0500

    android and ios name changed and milestone 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Meta Man',
+        title: 'MetaMan',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           fontFamily: 'Montserrat',
